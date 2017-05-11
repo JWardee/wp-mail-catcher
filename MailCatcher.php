09bf7912087a4c749b06ffac1f9dd66a1d16e808
James Ward <james@impact-designs.net>
2017-05-11 21:54:48 +0100

    +Added dummy table

--- MailCatcher.php
+++ MailCatcher.php
@@ -60,4 +60,39 @@ add_filter('wp_mail', function($args) {
     return $args;
 });
 
+add_action( 'admin_menu', function() {
+    add_menu_page('Mail Catcher', 'Mail Catcher', 'manage_options', 'mail-catcher', function() {
+            include __DIR__ . '/MailAdminTable.php';
+            //Create an instance of our package class...
+            $testListTable = new TT_Example_List_Table();
+            //Fetch, prepare, sort, and filter our data...
+            $testListTable->prepare_items();
+
+            ?>
+            <div class="wrap">
+
+                <div id="icon-users" class="icon32"><br/></div>
+                <h2>List Table Test</h2>
+
+                <div style="background:#ECECEC;border:1px solid #CCC;padding:0 10px;margin-top:5px;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px;">
+                    <p>This page demonstrates the use of the <tt><a href="http://codex.wordpress.org/Class_Reference/WP_List_Table" target="_blank" style="text-decoration:none;">WP_List_Table</a></tt> class in plugins.</p>
+                    <p>For a detailed explanation of using the <tt><a href="http://codex.wordpress.org/Class_Reference/WP_List_Table" target="_blank" style="text-decoration:none;">WP_List_Table</a></tt>
+                        class in your own plugins, you can view this file <a href="<?php echo admin_url( 'plugin-editor.php?plugin='.plugin_basename(__FILE__) ); ?>" style="text-decoration:none;">in the Plugin Editor</a> or simply open <tt style="color:gray;"><?php echo __FILE__ ?></tt> in the PHP editor of your choice.</p>
+                    <p>Additional class details are available on the <a href="http://codex.wordpress.org/Class_Reference/WP_List_Table" target="_blank" style="text-decoration:none;">WordPress Codex</a>.</p>
+                </div>
+
+                <!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
+                <form id="movies-filter" method="get">
+                    <!-- For plugins, we also need to ensure that the form posts back to our current page -->
+                    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+                    <!-- Now we can render the completed list table -->
+                    <?php $testListTable->display() ?>
+                </form>
+
+            </div>
+        <?php
+    });
+});
+
+
 
