291d94d9e9b0fcff7ccc27bf5ed94662a73e314e
James Ward <james@impact-designs.net>
2017-05-14 00:22:32 +0100

    +Added ordering and pagination

--- libs/MailAdminTable.php
+++ libs/MailAdminTable.php
@@ -107,8 +107,7 @@ class MailAdminTable extends WP_List_Table {
          * First, lets decide how many records per page to show
          */
         $per_page = 5;
-        
-        
+
         /**
          * REQUIRED. Now we need to define our column headers. This includes a complete
          * array of columns to be displayed (slugs & titles), a list of columns
@@ -128,91 +127,25 @@ class MailAdminTable extends WP_List_Table {
          * for sortable columns.
          */
         $this->_column_headers = array($columns, $hidden, $sortable);
-        
-        
-        /**
-         * Optional. You can handle your bulk actions however you see fit. In this
-         * case, we'll handle them within our package just to keep things clean.
-         */
         $this->process_bulk_action();
-        
-        
-        /**
-         * Instead of querying a database, we're going to fetch the example data
-         * property we created for use in this plugin. This makes this example 
-         * package slightly different than one you might build on your own. In 
-         * this example, we'll be using array manipulation to sort and paginate 
-         * our data. In a real-world implementation, you will probably want to 
-         * use sort and pagination data to build a custom query instead, as you'll
-         * be able to use your precisely-queried data immediately.
-         */
-        $data = $wpdb->get_results("SELECT id, emailto, subject, message, status, error FROM wp_mail_catcher_logs", ARRAY_A);//$this->example_data;
-                
-//        var_dump($data);
-//        exit;
 
-        /**
-         * This checks for sorting input and sorts the data in our array accordingly.
-         * 
-         * In a real-world situation involving a database, you would probably want 
-         * to handle sorting by passing the 'orderby' and 'order' values directly 
-         * to a custom query. The returned data will be pre-sorted, and this array
-         * sorting technique would be unnecessary.
-         */
-//        function usort_reorder($a,$b){
-//            $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'title'; //If no sort, default to title
-//            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
-//            $result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
-//            return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
-//        }
-//        usort($data, 'usort_reorder');
-        
-        
-        /***********************************************************************
-         * ---------------------------------------------------------------------
-         * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-         * 
-         * In a real-world situation, this is where you would place your query.
-         *
-         * For information on making queries in WordPress, see this Codex entry:
-         * http://codex.wordpress.org/Class_Reference/wpdb
-         * 
-         * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-         * ---------------------------------------------------------------------
-         **********************************************************************/
-        
-                
-        /**
-         * REQUIRED for pagination. Let's figure out what page the user is currently 
-         * looking at. We'll need this later, so you should always include it in 
-         * your own package classes.
-         */
-        $current_page = $this->get_pagenum();
-        
-        /**
-         * REQUIRED for pagination. Let's check how many items are in our data array. 
-         * In real-world use, this would be the total number of items in your database, 
-         * without filtering. We'll need this later, so you should always include it 
-         * in your own package classes.
-         */
-        $total_items = count($data);
-        
-        
-        /**
-         * The WP_List_Table class does not handle pagination for us, so we need
-         * to ensure that the data is trimmed to only the current page. We can use
-         * array_slice() to 
-         */
-        $data = array_slice($data,(($current_page-1)*$per_page),$per_page);
-        
-        
-        
-        /**
-         * REQUIRED. Now we can add our *sorted* data to the items property, where 
-         * it can be used by the rest of the class.
-         */
-        $this->items = $data;
-        
+        $sql = "SELECT id, emailto, subject, message, status, error
+                FROM " . $wpdb->prefix . MailCatcher::$table_name;
+
+        // TODO: Sanitise $_REQUEST
+        if (!empty($_REQUEST['orderby'])) {
+            $sql .= " ORDER BY " . $_REQUEST['orderby'];
+        }
+
+        if (!empty($_REQUEST['order'])) {
+            $sql .= " " . $_REQUEST['order'];
+        }
+
+        $sql .= " LIMIT " . $per_page . "
+                OFFSET " . ($per_page * ($this->get_pagenum() - 1));
+
+        $this->items = $wpdb->get_results($sql, ARRAY_A);
+        $total_items = $wpdb->get_var("SELECT COUNT(*) FROM " . $wpdb->prefix . MailCatcher::$table_name);
         
         /**
          * REQUIRED. We also have to register our pagination options & calculations.
