d5c387464859af05c520dc1e17dd3d10c6b660a9
James Ward <james@impact-designs.net>
2017-09-23 22:19:47 +0100

    +Added composer.json for local phpunit +Updated readme

--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
+# Mail Catcher
+
 [![Build Status](https://travis-ci.org/JWardee/mail-catcher.svg?branch=master)](https://travis-ci.org/JWardee/mail-catcher)
+
+Plugin detail goes here.
+
+## Testing locally
+
+Notes on local testing go here
+
+## Contributing
+
+Notes about contributing here
+
--- testing/tests/bootstrap.php
+++ testing/tests/bootstrap.php
@@ -5,15 +5,7 @@
  * @package Sample_Plugin
  */
 
-require __DIR__ . '/../../vendor/autoload.php';
-
-//print_r(dirname(__DIR__));
-//exit;
-
-//$_tests_dir = getenv( 'WP_TESTS_DIR' );
-//if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-//}
+$_tests_dir = '/tmp/wordpress-tests-lib';
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
@@ -25,9 +17,8 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/../MailCatcher.php';
 	$bootstrap->install();
-
-
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
