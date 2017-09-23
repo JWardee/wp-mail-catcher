8342cc50bbb5e27f4c15d623dcad76e01c4e6f32
James Ward <james@impact-designs.net>
2017-09-23 21:28:44 +0100

    +Update to travis

--- testing/tests/bootstrap.php
+++ testing/tests/bootstrap.php
@@ -12,7 +12,7 @@
 
 //$_tests_dir = getenv( 'WP_TESTS_DIR' );
 //if ( ! $_tests_dir ) {
-	$_tests_dir = dirname(__DIR__) . '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 //}
 
 // Give access to tests_add_filter() function.
